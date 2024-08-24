3121eb199cd2b9cb7eee468d7e8f938215e94e96
Dmitry <40324321+inilim@users.noreply.github.com>
2024-08-24 18:36:28 +0300

    ref

---
+++ src/Other.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Inilim\Env;
+
+class Other
+{
+    /**
+     * @return array{message:string,line:int,code:int,file:string,trace_string:string,class_string:class-string}
+     */
+    function getExceptionDetails(\Throwable $e): array
+    {
+        return [
+            'message'      => $e->getMessage(),
+            'line'         => $e->getLine(),
+            'code'         => $e->getCode(),
+            'file'         => $e->getFile(),
+            'trace_string' => $e->getTraceAsString(),
+            'class_string' => \get_class($e),
+        ];
+    }
+}
