0a8360409b4fe35359b9f8447fa52fca8e3472a3
Dmitry <inipel@ya.ru>
2024-09-04 19:13:51 +0300

    getReflectionClass getRefMethodsFromObjOrClass getNameMethodsFromObjOrClass

--- src/Other.php
+++ src/Other.php
@@ -26,6 +26,18 @@ class Other
         };
     }
 
+    function getReflectionClass(object|string $class_of_obj, bool $throw = false): ?\ReflectionClass
+    {
+        if (\is_string($class_of_obj)) {
+            if (!\class_exists($class_of_obj)) {
+                return $throw
+                    ? throw new \ReflectionException('class not found ' . $class_of_obj)
+                    : null;
+            }
+        }
+        return new \ReflectionClass($class_of_obj);
+    }
+
     /**
      * @return array{message:string,line:int,code:int,file:string,trace:string|array,class:class-string}
      */
@@ -39,5 +51,85 @@ class Other
             'trace'   => $trace_as_array ? $e->getTrace() : $e->getTraceAsString(),
             'class'   => \get_class($e),
         ];
+    }
+
+    /**
+     * @param object|class-string $class_of_obj
+     * @param string[] $except_methods
+     * @return \ReflectionMethod[]|array{}
+     */
+    function getRefMethodsFromObjOrClass(
+        object|string $class_of_obj,
+        array $except_methods          = [],
+        bool $except_magic_methods     = false,
+        bool $except_private_methods   = false,
+        bool $except_protected_methods = false,
+        bool $except_public_methods    = false,
+        bool $except_parent_methods    = false,
+    ): array {
+        $ref     = $this->getReflectionClass($class_of_obj, true);
+        $methods = $ref->getMethods();
+
+        if (!$methods) {
+            return [];
+        }
+
+        if ($methods && $except_parent_methods) {
+            $refParent = $ref->getParentClass();
+            if ($refParent) {
+                $parent_class = $refParent->name;
+                $methods = \array_filter($methods, static fn($m) => $m->class !== $parent_class);
+            }
+        }
+        unset($refParent, $parent_class, $ref);
+
+        if ($methods && $except_private_methods) {
+            $methods = \array_filter($methods, static fn($m) => !$m->isPrivate());
+        }
+
+        if ($methods && $except_protected_methods) {
+            $methods = \array_filter($methods, static fn($m) => !$m->isProtected());
+        }
+
+        if ($methods && $except_public_methods) {
+            $methods = \array_filter($methods, static fn($m) => !$m->isPublic());
+        }
+
+        if ($methods && $except_methods) {
+            $methods = \array_filter($methods, static fn($m) => !\in_array($m->name, $except_methods));
+        }
+
+        if ($methods && $except_magic_methods) {
+            $magic_methods = \_data()->magicMethodsAsArray();
+            $methods = \array_filter($methods, static fn($m) => !\in_array($m->name, $magic_methods));
+            unset($magic_methods);
+        }
+
+        return $methods;
+    }
+
+    /**
+     * @param object|class-string $class_of_obj
+     * @param string[] $except_methods
+     * @return string[]|array{}
+     */
+    function getNameMethodsFromObjOrClass(
+        object|string $class_of_obj,
+        array $except_methods          = [],
+        bool $except_magic_methods     = false,
+        bool $except_private_methods   = false,
+        bool $except_protected_methods = false,
+        bool $except_public_methods    = false,
+        bool $except_parent_methods    = false,
+    ): array {
+        return \array_column($this->getRefMethodsFromObjOrClass(
+            class_of_obj: $class_of_obj,
+            except_methods: $except_methods,
+            except_magic_methods: $except_magic_methods,
+            except_private_methods: $except_private_methods,
+            except_protected_methods: $except_protected_methods,
+            except_public_methods: $except_public_methods,
+            except_parent_methods: $except_parent_methods,
+        ), 'name');
     }
 }
--- test.php
+++ test.php
@@ -5,3 +5,39 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Inilim\Dump\Dump;
 
 Dump::init();
+
+class ParentClass
+{
+    function func() {}
+
+    function parent_pub() {}
+    protected function parent_prot() {}
+    private function parent_priv() {}
+
+    static function parent_static_pub() {}
+    static protected function parent_static_prot() {}
+    static private function parent_static_priv() {}
+}
+
+class ChildClass extends ParentClass
+{
+    function func() {}
+
+    function child_pub() {}
+    protected function child_prot() {}
+    private function child_priv() {}
+
+    static function child_static_pub() {}
+    static protected function child_static_prot() {}
+    static private function child_static_priv() {}
+}
+
+
+
+$obj = new ChildClass;
+$class = \Inilim\FuncOther\Other::class;
+
+
+$m = \_other()->getNameMethodsFromObjOrClass($obj, except_public_methods: true);
+
+de($m);
